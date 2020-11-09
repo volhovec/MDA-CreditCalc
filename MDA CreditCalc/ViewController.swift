b1134315035ebc61d49b68dc9f98c4d49d733c03
Stefanenko Leonid <stefanenko_lu@hotmail.com>
2020-11-09 12:39:47 +0300

    Initial Commit

---
+++ MDA CreditCalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MDA CreditCalc
+//
+//  Created by Leonid Stefanenko on 09.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
